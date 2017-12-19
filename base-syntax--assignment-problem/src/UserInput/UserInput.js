0a08084179bfdf96583cbc833a576cc6029d2ec2
tony2tones <anthonymarques@outlook.com>
2017-12-19 18:49:31 +0200

    setting up project and removing any clutter.

---
+++ base-syntax--assignment-problem/src/UserInput/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserInput.css';
+
+const userInput = () => {
+    return (
+        <div className="UserInput">
+            <input type="text" />
+        </div>
+    )
+}
+
+export default userInput;
