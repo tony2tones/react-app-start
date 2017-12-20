f98c0bb549720580f6d4d45cf7eb7d2b292a7b87
tony2tones <anthonymarques@outlook.com>
2017-12-20 18:13:57 +0200

    Added username via props and added state

--- base-syntax--assignment-problem/src/App.js
+++ base-syntax--assignment-problem/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    user: [
+      {username: 'Malone'},
+      {username: 'Tony2tones'},
+      {username: 'hopscotch'}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,9 +27,12 @@ class App extends Component {
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ol>
-        <br />
+        <hr />
 
         <UserInput />
+        <UserOutput name="tony"> </UserOutput>
+        <UserOutput name={this.state.user[1].username}/>
+        <UserOutput name={this.state.user[2].username}/>
       </div>
     );
   }
---
+++ base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+    <div className="UserOutput">
+        <p>{props.name} Lorem ispum Dawgs Lorem ispum Dawgs Lorem ispum Dawgs Lorem ispum Dawgs</p>
+        <p>Lorem ispum Dawgs Lorem ispum Dawgs Lorem ispum Dawgs Lorem ispum Dawgs</p>
+    </div>
+    )
+}
+
+export default userOutput;
