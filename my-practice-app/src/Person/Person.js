60f9880325420d6896cf61149edc410090f8a964
tony2tones <anthonymarques@outlook.com>
2017-12-27 12:38:17 +0200

    added Css styling for Person component

--- my-practice-app/src/Person/Person.js
+++ my-practice-app/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return <div className="Person">
         <p onClick={props.clickThisBaby}> My name is {props.name} and i'm {props.age} years old </p>
-        <input type="text" onChange={props.switchThisName}/>
+        <input type="text" onChange={props.switchThisName} value={props.name}/>
     </div>
 
 };
