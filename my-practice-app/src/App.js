cbe969adde6ae63a85a6dcf140a13071d4b97978
tony2tones <anthonymarques@outlook.com>
2017-12-27 11:53:15 +0200

    adding propss

--- my-practice-app/src/App.js
+++ my-practice-app/src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+       <Person name="Tony" age="32"/>
+       <Person name="malone" age="33"/>
        <Person />
       </div>
     );
--- my-practice-app/src/Person/Person.js
+++ my-practice-app/src/Person/Person.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const person = () => {
-    return 
-        <div>
-            <p> ola</p>
-        </div>
+const person = (props) => {
+    return <div className="Person">
+        <p> My name is {props.name} and i'm {props.age} years old </p>
+    </div>
 
-}
+};
 
 
 export default person;
